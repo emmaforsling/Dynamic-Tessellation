26b905503338860d7c0ed5292c2e23667287a826
Martin Gråd <martingrad@hotmail.com>
2016-04-28 13:35:33 +0200

    Simplified calculation of tessellation levels.

--- shaders/tessellation/tessellationcontrolshader.glsl
+++ shaders/tessellation/tessellationcontrolshader.glsl
@@ -20,53 +20,10 @@ uniform float tessScale;
 
 #define ID gl_InvocationID
 
-// If camera position is near, tesselate more
-float calculateInnerTessLevelFromCameraDistance()
-{
-    float distToCamera = length(cameraPos_ws - (fragPos_ws[0] + fragPos_ws[1] + fragPos_ws[2]) / 3.0 ) ;
-   	// Return tess level based on the measure. [TODO: rewrite!]
-    if(distToCamera < 40.0 * tessScale && distToCamera > 31.0 * tessScale)
-    {
-        return 2.0;
-    }
-    else if(distToCamera <= 31.0 * tessScale && distToCamera > 23.0 * tessScale)
-    {
-        return 3.0;
-    }
-    else if(distToCamera <= 23.0 * tessScale && distToCamera > 16.0 * tessScale)
-    {
-        return 4.0;
-    }
-    else if(distToCamera <= 16.0 * tessScale && distToCamera > 11.0 * tessScale)
-    {
-        return 5.0;
-    }
-    else if(distToCamera <= 11.0 * tessScale && distToCamera > 7.0 * tessScale)
-    {
-        return 6.0;
-    }
-    else if(distToCamera <= 7.0 * tessScale && distToCamera > 4.0 * tessScale)
-    {
-        return 7.0;
-    }
-    else if(distToCamera <= 4.0 * tessScale && distToCamera > 2.0 * tessScale)
-    {
-        return 8.0;
-    }
-    else if(distToCamera <= 2.0 * tessScale && distToCamera > 1.0 * tessScale)
-    {
-        return 9.0;
-    }
-    else if(distToCamera <= 1.0 * tessScale)
-    {
-        return 10.0;
-    }
-    else
-    {
-        return 1.0;
-    }
-}
-
+/*
+*	Function innerTessLvl_screenSize calculates a tessellation level based on a measure representing
+*	the size of the patch on the screen.
+*/
 float innerTessLvl_screenSize()
 {
 	// Calculate area of the triangle
@@ -85,58 +42,7 @@ float innerTessLvl_screenSize()
 	float screenSizeMeasure = area / cameraDistance;
 
 	// Return tess level based on the measure. [TODO: rewrite!]
-	if(screenSizeMeasure < 0.005)
-	{
-		return 1.0;
-	}
-	else if(screenSizeMeasure >= 0.005 && screenSizeMeasure < 0.01)
-	{
-		return 2.0;
-	}
-	else if(screenSizeMeasure >= 0.01 && screenSizeMeasure < 0.03)
-	{
-		return 3.0;
-	}
-	else if(screenSizeMeasure >= 0.03 && screenSizeMeasure < 0.035)
-	{
-		return 3.0;
-	}
-	else if(screenSizeMeasure >= 0.035 && screenSizeMeasure < 0.040)
-	{
-		return 4.0;
-	}
-	else if(screenSizeMeasure >= 0.040 && screenSizeMeasure < 0.045)
-	{
-		return 5.0;
-	}
-	else if(screenSizeMeasure >= 0.045 && screenSizeMeasure < 0.050)
-	{
-		return 6.0;
-	}
-	else if(screenSizeMeasure >= 0.050 && screenSizeMeasure < 0.055)
-	{
-		return 7.0;
-	}
-	else if(screenSizeMeasure >= 0.055 && screenSizeMeasure < 0.060)
-	{
-		return 8.0;
-	}
-	else if(screenSizeMeasure >= 0.060 && screenSizeMeasure < 0.065)
-	{
-		return 9.0;
-	}
-	else if(screenSizeMeasure >= 0.065 && screenSizeMeasure < 0.070)
-	{
-		return 10.0;
-	}
-	else if(screenSizeMeasure >= 0.070 && screenSizeMeasure < 0.1)
-	{
-		return 11.0;
-	}
-	else if(screenSizeMeasure >= 0.1)
-	{
-		return 12.0;
-	}
+	return 1.0 + floor(screenSizeMeasure * 100.0 * tessScale);
 }
 
 
@@ -167,46 +73,7 @@ float outerTessLvl_screenSize(int _vIdx0, int _vIdx1)
 	float screenSizeMeasure = edgeLength * visibilityMeasure / cameraDistance;
 
 	// Return tess level based on the measure. [TODO: rewrite!]
-	if(screenSizeMeasure >= 0.025 && screenSizeMeasure < 0.08)
-	{
-		return 2.0;
-	}
-	if(screenSizeMeasure >= 0.08 && screenSizeMeasure < 0.12)
-	{
-		return 3.0;
-	}
-	if(screenSizeMeasure >= 0.12 && screenSizeMeasure < 0.16)
-	{
-		return 4.0;
-	}
-	if(screenSizeMeasure >= 0.16 && screenSizeMeasure < 0.20)
-	{
-		return 5.0;
-	}
-	if(screenSizeMeasure >= 0.20 && screenSizeMeasure < 0.24)
-	{
-		return 6.0;
-	}
-	if(screenSizeMeasure >= 0.24 && screenSizeMeasure < 0.32)
-	{
-		return 7.0;
-	}
-	if(screenSizeMeasure >= 0.32 && screenSizeMeasure < 0.44)
-	{
-		return 8.0;
-	}
-	if(screenSizeMeasure >= 0.44 && screenSizeMeasure < 0.56)
-	{
-		return 9.0;
-	}
-	if(screenSizeMeasure >= 0.56)
-	{
-		return 10.0;
-	}
-	else
-	{
-		return 1.0;
-	}
+	return 1.0 + floor(screenSizeMeasure * 25.0 * tessScale);
 }
 
 void main()
